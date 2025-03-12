7d28ca652e20ef2d4661f52c4a8d50272d56aee5
Nazia-Hoda <nazia.hodaansari@deepmindz.co>
2025-03-12 16:57:45 +0530

    new reaame added

--- src/Components/Chatboat.jsx
+++ src/Components/Chatboat.jsx
@@ -184,7 +184,7 @@ function Chatboat() {
         {/* Chatbot Gpt-4o */}
         <div className="flex-1 border  h-[90vh] border-gray-300 rounded-lg shadow-lg bg-white">
         <div className="flex justify-between items-center w-full gap-2 p-2">
-  <h2 className="text-xl font-bold bg-gray-400 text-gray-800 py-2 px-4 flex-1 text-center rounded-lg shadow-md">NSE</h2>
+  <h2 className="text-xl font-bold bg-gray-400 text-gray-800 py-2 px-4 flex-1 text-center rounded-lg shadow-md">Fiance</h2>
   {/* <div className="relative w-1/4">
     <select className="text-md font-bold bg-gray-400 text-gray-800 py-2 px-4 rounded-lg shadow-md w-full appearance-none focus:outline-none focus:ring-2 focus:ring-gray-500 focus:shadow-lg"
     onChange={(e) => {
